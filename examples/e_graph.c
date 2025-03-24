d57013c60d4f9bdf2ec001fde48a5a186cc8a786
jasperdevir <143695646+jasperdevir@users.noreply.github.com>
2025-03-24 20:01:54 +1100

    Finialised graph example

--- examples/e_graph.c
+++ examples/e_graph.c
@@ -1,48 +1,92 @@
-#define STD_DDS_ERROR_MSG
+/* std_dds - Standard Dynamic Data Structures
+ * Copyright (C) 2025 Jasper Devir <jasperdevir.jd@gmail.com>
+ *
+ * std_dds is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * std_dds is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with std_dds.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include "graph.h"
 
 #include <stdio.h>
 
 int main(void){
+    printf("\n== std_dds Graph Example ==\n");
+
+    printf("\n-- GraphInit() --\n");
+    
+    unsigned int vLength = 4;
+
+    printf("Initialising a Graph with %d vertices.\n", vLength);
     Graph *graph = GraphInit(4);
+
+    printf("\n-- GraphInsertEdge() --\n");
 
     Edge a;
     a.v = 0;
     a.w = 1;
+    printf("Inserting edge, '%d - %d` into Graph.\n", a.v, a.w);    
     GraphInsertEdge(graph, a, false);
 
     Edge b;
     b.v = 0;
     b.w = 3;
+    printf("Inserting edge, '%d - %d` into Graph.\n", b.v, b.w);    
     GraphInsertEdge(graph, b, false);
 
     Edge c;
     c.v = 1;
     c.w = 3;
+    printf("Inserting edge, '%d - %d` into Graph.\n", c.v, c.w);    
     GraphInsertEdge(graph, c, false);
 
     Edge d;
     d.v = 2;
     d.w = 3;
+    printf("Inserting edge, '%d - %d` into Graph.\n", d.v, d.w);    
     GraphInsertEdge(graph, d, false);
 
     Edge e;
     e.v = 1;
     e.w = 2;
+    printf("Inserting edge, '%d - %d` into Graph.\n", e.v, e.w);    
     GraphInsertEdge(graph, e, false);
 
+    printf("\n-- GraphPrint() --\n");
     GraphPrint(graph, false); 
 
-    Edge *edge = GraphGetEdge(graph, 1, 2);
+    printf("\n-- GraphGetEdge() --\n"); 
+
+    unsigned int eV = 1;
+    unsigned int eW = 2;
+
+    printf("Getting edge, '%d - %d' from Graph.\n", eV, eW);
+    Edge *edge = GraphGetEdge(graph, eV, eW);
     if(edge == NULL){
         printf("GraphGetEdge failed. Returned NULL.\n");
         exit(1);
     }
+    printf("Edge successfully returned: '%p'\n", edge);
 
+    printf("\n-- GraphRemoveEdge() --\n");
+
+    printf("Removing edge, '%d - %d' from Graph.\n", eV, eW);
     GraphRemoveEdge(graph,edge, false);
 
+    printf("Result:\n");
     GraphPrint(graph, false);
 
+    printf("\n-- GraphFree() --\n");
+    printf("Freeing Graph.\n");
     GraphFree(graph);
 
     return 0;
--- src/array_list.c
+++ src/array_list.c
@@ -13,229 +13,263 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with std_dds.  If not, see <https://www.gnu.org/licenses/>.
-*/
+ */
 
 #include "array_list.h"
 
 typedef struct arrayList {
-    void **values;
-    unsigned int capacity;
-    unsigned int length;
+  void **values;
+  unsigned int capacity;
+  unsigned int length;
 } ArrayList;
 
-ArrayList *ArrayListInit(unsigned int capacity){
-    ArrayList *list = (ArrayList *)malloc(sizeof(ArrayList));
-    if(list == NULL){
-        #ifdef STD_DDS_ERROR_MSG
-            fprintf(stderr, "[Error] ArrayList malloc failed. Unable to allocate memory of %zu bytes. Exiting.\n", sizeof(ArrayList));
-        #endif
-        exit(1);
-    }
+ArrayList *ArrayListInit(unsigned int capacity) {
+  ArrayList *list = (ArrayList *)malloc(sizeof(ArrayList));
+  if (list == NULL) {
+#ifdef STD_DDS_ERROR_MSG
+    fprintf(stderr,
+            "[Error] ArrayList malloc failed. Unable to allocate memory of %zu "
+            "bytes. Exiting.\n",
+            sizeof(ArrayList));
+#endif
+    exit(1);
+  }
 
-    list->values = (void **)malloc(sizeof(void *) * capacity);
-    if(list->values == NULL){
-        #ifdef STD_DDS_ERROR_MSG
-            fprintf(stderr, "[Error] ArrayList values malloc failed. Unable to allocate memory of %zu bytes. Exiting.\n", sizeof(void *) * capacity);
-        #endif
-        exit(1);
-    }
+  list->values = (void **)malloc(sizeof(void *) * capacity);
+  if (list->values == NULL) {
+#ifdef STD_DDS_ERROR_MSG
+    fprintf(stderr,
+            "[Error] ArrayList values malloc failed. Unable to allocate memory "
+            "of %zu bytes. Exiting.\n",
+            sizeof(void *) * capacity);
+#endif
+    exit(1);
+  }
 
-    list->length = 0;
-    list->capacity = capacity;
+  list->length = 0;
+  list->capacity = capacity;
 
-    return list;
+  return list;
 }
 
-void ArrayListResize(ArrayList *list, unsigned int capacity){
-    if(list == NULL){
-        #ifdef STD_DDS_WARNING_MSG
-            fprintf(stdout, "[Warning] ArrayListResize failed. ArrayList value is NULL.\n");
-        #endif
-        return;
-    }
+void ArrayListResize(ArrayList *list, unsigned int capacity) {
+  if (list == NULL) {
+#ifdef STD_DDS_WARNING_MSG
+    fprintf(stdout,
+            "[Warning] ArrayListResize failed. ArrayList value is NULL.\n");
+#endif
+    return;
+  }
 
-    void **values = (void **)realloc(list->values, sizeof(void *) * capacity);
-    if(values == NULL){
-        #ifdef STD_DDS_ERROR_MSG
-            fprintf(stderr, "[Error] ArrayList values realloc failed. Unable to reallocate memory of %zu bytes. Exiting.\n", sizeof(void *) * capacity);
-        #endif
-        exit(1);
-    }
+  void **values = (void **)realloc(list->values, sizeof(void *) * capacity);
+  if (values == NULL) {
+#ifdef STD_DDS_ERROR_MSG
+    fprintf(stderr,
+            "[Error] ArrayList values realloc failed. Unable to reallocate "
+            "memory of %zu bytes. Exiting.\n",
+            sizeof(void *) * capacity);
+#endif
+    exit(1);
+  }
 
-    list->values = values;
+  list->values = values;
 
-    list->capacity = capacity;
+  list->capacity = capacity;
 
-    if(list->length > list->capacity){
-        #ifdef STD_DDS_WARNING_MSG
-            fprintf(stdout, "[Warning] ArrayList resized to greater than current length. Current length: '%d' -> '%d'.\n", list->length, list->capacity);
-        #endif
-        list->length = list->capacity;
-    }
+  if (list->length > list->capacity) {
+#ifdef STD_DDS_WARNING_MSG
+    fprintf(stdout,
+            "[Warning] ArrayList resized to greater than current length. "
+            "Current length: '%d' -> '%d'.\n",
+            list->length, list->capacity);
+#endif
+    list->length = list->capacity;
+  }
 }
 
-void *ArrayListGetAt(ArrayList *list, unsigned int index){
-    if(list == NULL){
-        #ifdef STD_DDS_WARNING_MSG
-            fprintf(stdout, "[Warning] ArrayListGetAt failed. ArrayList value is NULL. Returning NULL.\n");
-        #endif
-        return NULL;
-    }
+void *ArrayListGetAt(ArrayList *list, unsigned int index) {
+  if (list == NULL) {
+#ifdef STD_DDS_WARNING_MSG
+    fprintf(stdout, "[Warning] ArrayListGetAt failed. ArrayList value is NULL. "
+                    "Returning NULL.\n");
+#endif
+    return NULL;
+  }
 
-    if(list->length <= 0 || index >= list->length){
-        #ifdef STD_DDS_WARNING_MSG
-            fprintf(stdout, "[Warning] Index '%d' is out-of-bounds for ArrayList with a length of '%d'.\n", index, list->length);
-        #endif
-        return NULL;
-    }
+  if (list->length <= 0 || index >= list->length) {
+#ifdef STD_DDS_WARNING_MSG
+    fprintf(stdout,
+            "[Warning] Index '%d' is out-of-bounds for ArrayList with a length "
+            "of '%d'.\n",
+            index, list->length);
+#endif
+    return NULL;
+  }
 
-    return list->values[index];
+  return list->values[index];
 }
 
-void ArrayListPush(ArrayList *list, void *value){
-    if(list == NULL){
-        #ifdef STD_DDS_WARNING_MSG
-            fprintf(stdout, "[Warning] ArrayListPush failed. ArrayList value is NULL.\n");
-        #endif
-        return;
-    }
+void ArrayListPush(ArrayList *list, void *value) {
+  if (list == NULL) {
+#ifdef STD_DDS_WARNING_MSG
+    fprintf(stdout,
+            "[Warning] ArrayListPush failed. ArrayList value is NULL.\n");
+#endif
+    return;
+  }
 
-    if(list->length >= list->capacity){
-        #ifdef STD_DDS_WARNING_MSG
-            fprintf(stdout, "[Warning] ArrayList is at capacity, resizing from '%d' to '%d'.\n", list->capacity, list->capacity * 2);
-        #endif
-        ArrayListResize(list, list->capacity * 2);
-    }
+  if (list->length >= list->capacity) {
+#ifdef STD_DDS_WARNING_MSG
+    fprintf(stdout,
+            "[Warning] ArrayList is at capacity, resizing from '%d' to '%d'.\n",
+            list->capacity, list->capacity * 2);
+#endif
+    ArrayListResize(list, list->capacity * 2);
+  }
 
-    for (int i = list->length - 1; i >= 0; i--) {
-        list->values[i + 1] = list->values[i];
-    }
+  for (int i = list->length - 1; i >= 0; i--) {
+    list->values[i + 1] = list->values[i];
+  }
 
-    list->values[0] = value;    
-    list->length++;
-
+  list->values[0] = value;
+  list->length++;
 }
 
-void ArrayListAppend(ArrayList *list, void *value){
-    if(list == NULL){
-        #ifdef STD_DDS_WARNING_MSG
-            fprintf(stdout, "[Warning] ArrayListAppend failed. ArrayList value is NULL.\n");
-        #endif
-        return;
-    }
+void ArrayListAppend(ArrayList *list, void *value) {
+  if (list == NULL) {
+#ifdef STD_DDS_WARNING_MSG
+    fprintf(stdout,
+            "[Warning] ArrayListAppend failed. ArrayList value is NULL.\n");
+#endif
+    return;
+  }
 
-    if(list->length >= list->capacity){
-        #ifdef STD_DDS_WARNING_MSG
-            fprintf(stdout, "[Warning] ArrayList is at capacity, resizing from '%d' to '%d'.\n", list->capacity, list->capacity * 2);
-        #endif
-        ArrayListResize(list, list->capacity * 2);
-    }
+  if (list->length >= list->capacity) {
+#ifdef STD_DDS_WARNING_MSG
+    fprintf(stdout,
+            "[Warning] ArrayList is at capacity, resizing from '%d' to '%d'.\n",
+            list->capacity, list->capacity * 2);
+#endif
+    ArrayListResize(list, list->capacity * 2);
+  }
 
-    list->values[list->length] = value;
+  list->values[list->length] = value;
 
-    list->length++;
+  list->length++;
 }
 
-void ArrayListInsertAt(ArrayList *list, void *value, unsigned int index){
-    if(list == NULL){
-        #ifdef STD_DDS_WARNING_MSG
-            fprintf(stdout, "[Warning] ArrayListInsertAt failed. ArrayList value is NULL.\n");
-        #endif
-        return;
-    }
+void ArrayListInsertAt(ArrayList *list, void *value, unsigned int index) {
+  if (list == NULL) {
+#ifdef STD_DDS_WARNING_MSG
+    fprintf(stdout,
+            "[Warning] ArrayListInsertAt failed. ArrayList value is NULL.\n");
+#endif
+    return;
+  }
 
-    if(list->length >= list->capacity){
-        #ifdef STD_DDS_WARNING_MSG
-            fprintf(stdout, "[Warning] ArrayList is at capacity, resizing from '%d' to '%d'.\n", list->capacity, list->capacity * 2);
-        #endif
-        ArrayListResize(list, list->capacity * 2);
-    }
+  if (list->length >= list->capacity) {
+#ifdef STD_DDS_WARNING_MSG
+    fprintf(stdout,
+            "[Warning] ArrayList is at capacity, resizing from '%d' to '%d'.\n",
+            list->capacity, list->capacity * 2);
+#endif
+    ArrayListResize(list, list->capacity * 2);
+  }
 
-    for(int i = list->length - 1; i >= index; i--){
-        list->values[i + 1] = list->values[i];
-    }
+  for (int i = list->length - 1; i >= index; i--) {
+    list->values[i + 1] = list->values[i];
+  }
 
-    list->values[index] = value;
-    list->length++;
+  list->values[index] = value;
+  list->length++;
 }
 
-void *ArrayListPop(ArrayList *list){
-   if(list == NULL){
-        #ifdef STD_DDS_WARNING_MSG
-            fprintf(stdout, "[Warning] ArrayListPop failed. ArrayList value is NULL. Returning NULL.\n");
-        #endif
-        return NULL;
-    } 
+void *ArrayListPop(ArrayList *list) {
+  if (list == NULL) {
+#ifdef STD_DDS_WARNING_MSG
+    fprintf(stdout, "[Warning] ArrayListPop failed. ArrayList value is NULL. "
+                    "Returning NULL.\n");
+#endif
+    return NULL;
+  }
 
-    if(list->length <= 0){
-        #ifdef STD_DDS_WARNING_MSG
-            fprintf(stdout, "[Warning] Unable to pop ArrayList as its current length is 0. Returning NULL.\n");
-        #endif
-        return NULL;
-    }
+  if (list->length <= 0) {
+#ifdef STD_DDS_WARNING_MSG
+    fprintf(stdout, "[Warning] Unable to pop ArrayList as its current length "
+                    "is 0. Returning NULL.\n");
+#endif
+    return NULL;
+  }
 
-    list->length--;
+  list->length--;
 
-    return list->values[list->length];
+  return list->values[list->length];
 }
 
-void *ArrayListRemoveAt(ArrayList *list, unsigned int index){
-    if(list == NULL){
-        #ifdef STD_DDS_WARNING_MSG
-            fprintf(stdout, "[Warning] ArrayListRemoveAt failed. ArrayList value is NULL. Returning NULL.\n");
-        #endif
-        return NULL;
-    }
+void *ArrayListRemoveAt(ArrayList *list, unsigned int index) {
+  if (list == NULL) {
+#ifdef STD_DDS_WARNING_MSG
+    fprintf(stdout, "[Warning] ArrayListRemoveAt failed. ArrayList value is "
+                    "NULL. Returning NULL.\n");
+#endif
+    return NULL;
+  }
 
-    if(index >= list->length){
-        #ifdef STD_DDS_WARNING_MSG
-            fprintf(stdout, "[Warning] Index '%d' is out-of-bounds for ArrayList with a length of '%d'. Returning NULL.\n", index, list->length);
-        #endif
-        return NULL;
-    }
+  if (index >= list->length) {
+#ifdef STD_DDS_WARNING_MSG
+    fprintf(stdout,
+            "[Warning] Index '%d' is out-of-bounds for ArrayList with a length "
+            "of '%d'. Returning NULL.\n",
+            index, list->length);
+#endif
+    return NULL;
+  }
 
-    void *value = list->values[index];
+  void *value = list->values[index];
 
-    for(unsigned int i = index; i < list->length - 1; i++){
-        list->values[index] = list->values[index + 1];
-    }
+  for (unsigned int i = index; i < list->length - 1; i++) {
+    list->values[index] = list->values[index + 1];
+  }
 
-    list->length--;
+  list->length--;
 
-    return value;
+  return value;
 }
 
-unsigned int ArrayListGetLength(ArrayList *list){
-    if(list == NULL){
-        #ifdef STD_DDS_WARNING_MSG
-            fprintf(stdout, "[Warning] ArrayListGetLength failed. ArrayList value is NULL. Returning 0.\n");
-        #endif
-        return 0;
-    }
+unsigned int ArrayListGetLength(ArrayList *list) {
+  if (list == NULL) {
+#ifdef STD_DDS_WARNING_MSG
+    fprintf(stdout, "[Warning] ArrayListGetLength failed. ArrayList value is "
+                    "NULL. Returning 0.\n");
+#endif
+    return 0;
+  }
 
-    return list->length;
+  return list->length;
 }
 
-unsigned int ArrayListGetCapacity(ArrayList *list){
-    if(list == NULL){
-        #ifdef STD_DDS_WARNING_MSG
-            fprintf(stdout, "[Warning] ArrayListGetCapacity failed. ArrayList value is NULL. Returning 0.\n");
-        #endif
-        return 0;
-    }
+unsigned int ArrayListGetCapacity(ArrayList *list) {
+  if (list == NULL) {
+#ifdef STD_DDS_WARNING_MSG
+    fprintf(stdout, "[Warning] ArrayListGetCapacity failed. ArrayList value is "
+                    "NULL. Returning 0.\n");
+#endif
+    return 0;
+  }
 
-    return list->capacity;
+  return list->capacity;
 }
 
-void ArrayListFree(ArrayList *list){
-    if(list == NULL){
-        #ifdef STD_DDS_WARNING_MSG
-            fprintf(stdout, "[Warning] ArrayListFree failed. ArrayList value is NULL.\n");
-        #endif
-        return;
-    }
-    
-    free(list->values);
+void ArrayListFree(ArrayList *list) {
+  if (list == NULL) {
+#ifdef STD_DDS_WARNING_MSG
+    fprintf(stdout,
+            "[Warning] ArrayListFree failed. ArrayList value is NULL.\n");
+#endif
+    return;
+  }
 
-    free(list);
+  free(list->values);
+
+  free(list);
 }
